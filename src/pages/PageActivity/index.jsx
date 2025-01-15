6c7b39749522884ecb79b512a67ca7699f5f1409
Nguyễn Tuấn Anh <anh.nt211202@gmail.com>
2025-01-15 20:09:48 +0700

    Update activity

--- src/pages/PageActivity/index.jsx
+++ src/pages/PageActivity/index.jsx
@@ -18,8 +18,6 @@ const PageActivity = () => {
     }
   });
 
-  console.log(activities);
-
   const defaultText = (createAt) => {
     if (!createAt) return;
     const date = new Date(createAt);
@@ -43,17 +41,17 @@ const PageActivity = () => {
         case 2:
           return "You have deleted a post.";
         case 3: 
-          return "You have commented on a post.";
+          return `You have commented on ${item?.post?.bookName.slice(0, 10)}...`;
         case 4: 
-          return "You have liked a post.";
+          return `You have reacted to ${item?.post?.bookName.slice(0, 10)}...`;
         case 5:
-          return "You have replied on a comment.";
+          return `You have replied on @${item.comment?.comment?.author?.username}: ${item.comment?.comment?.content.slice(0, 15)}...`;
         case 6:
-          return "You have liked a comment.";
+          return `You have reacted to a comment.`;
         case 13:
-          return "You have unliked a post.";
+          return `You have unreacted to ${item?.post?.bookName.slice(0, 10)}...`;
         case 14:
-          return "You have unliked a comment.";
+          return "You have unreacted to a comment.";
         case 7:
           return "You have created a note.";
         default:
@@ -61,28 +59,20 @@ const PageActivity = () => {
       }
     }
 
-    const renderText2 = (type, post) => {
+    const renderText2 = (type, _item) => {
       switch (type) {
         case 0:
-          return post?.bookName.slice(0, 20) + (post?.bookName.length > 20 ? "..." : "");
         case 1:
-          return "You have updated a post.";
         case 2:
-          return "You have deleted a post.";
-        case 3: 
-          return "You have commented on a post.";
-        case 4: 
-          return "You have liked a post.";
+          return _item?.bookName.slice(0, 20) + (_item?.bookName.length > 20 ? "..." : "");
+        case 3:
         case 5:
-          return "You have replied on a comment.";
+          return _item?.content;
+        case 4:
         case 6:
-          return "You have liked a comment.";
         case 13:
-          return "You have unliked a post.";
         case 14:
-          return "You have unliked a comment.";
         case 7:
-          return "";
         default:
           return "";
       }
@@ -90,26 +80,42 @@ const PageActivity = () => {
 
     if (mode === 0) {
       return <div className="w-full flex gap-3 hover:cursor-pointer hover:bg-slate-100">
-        <img className="w-[15%] aspect-2/3" src={baseURL+item.post?.image} />
+        {item.type !== 7 ? <img className="w-[15%] aspect-2/3 rounded-md" src={baseURL+item.post?.image} /> : <img className="w-[15%] aspect-square object-cover object-fit rounded-md" src={baseURL+item.author.avatar} />}
         <div className="flex-grow flex flex-col justify-center text-xs md:text-base">
           <div>{renderText1(item.type)}</div>
-          <div>{renderText2(item.type, item.post)}</div>
-          <div>{defaultText(item.createdAt)}</div>
+          <div className="italic font-semibold">{renderText2(item.type, item.post)}</div>
+          <div className="font-light">{defaultText(item.createdAt)}</div>
         </div>
       </div>;
     } else if (mode === 1) {
-      return "comment";
+      return <div className="w-full flex gap-3 hover:cursor-pointer hover:bg-slate-100">
+        <img className="w-[15%] aspect-2/3 rounded-md" src={baseURL+item.post?.image} />
+        <div className="flex-grow flex flex-col justify-center text-xs md:text-base">
+          <div>{renderText1(item.type)}</div>
+          <div className="italic font-semibold">{renderText2(item.type, item.comment)}</div>
+          <div className="font-light">{defaultText(item.createdAt)}</div>
+        </div>
+      </div>;
     } else if (mode === 2) {
-      return "like";
+      return <div className="w-full flex gap-3 hover:cursor-pointer hover:bg-slate-100">
+        <img className="w-[15%] aspect-2/3 rounded-md" src={baseURL+item.post?.image} />
+        <div className="flex-grow flex flex-col justify-center text-xs md:text-base">
+          <div>{renderText1(item.type)}</div>
+          <div className="italic font-semibold">{renderText2(item.type, item.comment)}</div>
+          <div className="font-light">{defaultText(item.createdAt)}</div>
+        </div>
+      </div>;
     } else return <></>;
   }
 
   const renderByMode = (list) => {
-    if (mode === 0) return list?.filter((item) => !item.comment);
-    else if (mode === 1) return list?.filter((item) => !!item.comment && !!item.comment.content);
-    else if (mode === 2) return list?.filter((item) => !!item.comment && !item.comment.content);
+    if (mode === 0) return list?.filter((item) => [0,1,2,7].includes(item?.type));
+    else if (mode === 1) return list?.filter((item) => [3,5].includes(item?.type));
+    else if (mode === 2) return list?.filter((item) => [4,6,13,14].includes(item?.type));
     else return list;
   }
+
+  console.log(renderByMode(activities?.data), activities?.data);
 
   return (
     <div className="w-full h-screen p-3 flex flex-col gap-3 items-center">
@@ -119,7 +125,7 @@ const PageActivity = () => {
           <div className={"w-1/3 flex items-center justify-center pb-3 hover:cursor-pointer border-b-[1px] border-black " + (mode === 1 ? "" : "opacity-30")} onClick={() => setMode(1)}>Comment</div>
           <div className={"w-1/3 flex items-center justify-center pb-3 hover:cursor-pointer border-b-[1px] border-black " + (mode === 2 ? "" : "opacity-30")} onClick={() => setMode(2)}>React</div>
         </div>
-        <div className="w-full flex-grow overflow-y-scroll flex flex-col gap-3 pt-3">
+        <div className="w-full h-[calc(100%-45px)] overflow-y-scroll flex flex-col gap-3 pt-3">
           {renderByMode(activities?.data)?.map((item) => renderActivity(item))}
         </div>
       </div>
